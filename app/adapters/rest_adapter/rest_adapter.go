de0b4cf5015500f10d76aa64655fa0b2a8a47edd
jedi4z <jesusdiazbc@gmail.com>
2020-07-08 13:49:09 -0300

    add create game endpoint

--- app/adapters/rest_adapter/rest_adapter.go
+++ app/adapters/rest_adapter/rest_adapter.go
@@ -3,6 +3,7 @@ package rest_adapter
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/jedi4z/minesweeper-api/app/container"
+	"github.com/jedi4z/minesweeper-api/app/models"
 	"net/http"
 )
 
@@ -17,6 +18,7 @@ func NewRestEngine(c container.Container) *gin.Engine {
 	v1 := r.Group("/v1")
 	{
 		v1.GET("/ping", s.pingHandler)
+		v1.POST("/games", s.createGameHandler)
 	}
 
 	return r
@@ -24,4 +26,20 @@ func NewRestEngine(c container.Container) *gin.Engine {
 
 func (r RestAdapter) pingHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": "pong"})
+}
+
+func (r RestAdapter) createGameHandler(c *gin.Context) {
+	game := new(models.Game)
+
+	if err := c.ShouldBindJSON(game); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := r.container.GameUseCases.CreateGame(game); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, game)
 }
