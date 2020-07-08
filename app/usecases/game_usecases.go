dfdbaf4644f823828ffcf231772948d2693831bc
jedi4z <jesusdiazbc@gmail.com>
2020-07-08 13:37:21 -0300

    add game use cases

---
+++ app/usecases/game_usecases.go
@@ -0,0 +1,28 @@
+package usecases
+
+import (
+	"github.com/jedi4z/minesweeper-api/app/models"
+	"github.com/jedi4z/minesweeper-api/app/repositories"
+)
+
+type GameUseCases struct {
+	GameRepository repositories.GameRepositoryInterface
+}
+
+func NewGameUseCases(r repositories.GameRepositoryInterface) GameUseCasesInterface {
+	return GameUseCases{
+		GameRepository: r,
+	}
+}
+
+func (uc GameUseCases) CreateGame(game *models.Game) error {
+	return uc.GameRepository.Insert(game)
+}
+
+func (uc GameUseCases) ListGames() ([]models.Game, error) {
+	return uc.GameRepository.FindAll()
+}
+
+func (uc GameUseCases) GetGame(id uint) (*models.Game, error) {
+	return uc.GameRepository.Find(id)
+}
