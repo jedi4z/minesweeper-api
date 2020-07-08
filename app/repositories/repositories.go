285edfb84ee0d324aea46a5d22724ec4ca1599ff
jedi4z <jesusdiazbc@gmail.com>
2020-07-08 13:31:54 -0300

    add usecases and repositories interface

---
+++ app/repositories/repositories.go
@@ -0,0 +1,9 @@
+package repositories
+
+import "github.com/jedi4z/minesweeper-api/app/models"
+
+type GameRepositoryInterface interface {
+	Insert(game *models.Game) error
+	FindAll() ([]models.Game, error)
+	Find(id uint) (*models.Game, error)
+}
---
+++ app/usecases/usecases.go
@@ -0,0 +1,9 @@
+package usecases
+
+import "github.com/jedi4z/minesweeper-api/app/models"
+
+type GameUseCasesInterface interface {
+	CreateGame(game *models.Game) error
+	ListGames() ([]models.Game, error)
+	GetGame(id uint) (*models.Game, error)
+}
